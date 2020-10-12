7056b7ba5d3e2448bc8ba21dd726174efd60f7ae
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-10-12 09:26:41 +0300

    java master egitimi basladi

---
+++ week_0001/src/main/java/Main.java
@@ -0,0 +1,11 @@
+/*
+ * created by Muhammed Topgul
+ * on 12/10/2020
+ * at 09:22
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Muhammed");
+    }
+}
