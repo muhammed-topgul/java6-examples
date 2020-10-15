d090bc6d8de07c33ac87d2d261afb99af2e5c86c
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-10-15 10:26:34 +0300

    polymorphism islendi

---
+++ week_0001/src/main/java/_05_polymorphism/Main.java
@@ -0,0 +1,14 @@
+package _05_polymorphism;
+
+/*
+ * created by Muhammed Topgul
+ * on 15/10/2020
+ * at 09:44
+ */
+
+public class Main {
+}
+
+// çok biçimlilik
+// static -> overload // early binding
+// dynamic -> override // late binding // extends yada implements durumlarında gecerlidir
---
+++ week_0001/src/main/java/_05_polymorphism/_overload/Main.java
@@ -0,0 +1,18 @@
+package _05_polymorphism._overload;
+
+/*
+ * created by Muhammed Topgul
+ * on 15/10/2020
+ * at 09:49
+ */
+
+public class Main {
+}
+
+// method isimleri aynı
+// parametre listesi degismeli sayi veya tip olarak
+// amac reusable methodlar yazmak
+// overload methodlarda donus tipi desgisebilir
+// donus tipi degisse bile parametreler mutlaka degismelidir
+// access level degisebilir
+// method ve constructor icin gecerlidir
