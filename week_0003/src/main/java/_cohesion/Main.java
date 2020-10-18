bd32c780133e62a9e5b245f93e23baa05295f558
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-10-18 13:31:40 +0300

    coupling ve cohesion konuları islendi

---
+++ week_0003/src/main/java/_cohesion/Main.java
@@ -0,0 +1,17 @@
+package _cohesion;
+
+/*
+ * created by Muhammed Topgul
+ * on 18/10/2020
+ * at 13:20
+ */
+
+public class Main {
+}
+
+// high COHESION istenen bir durumdur
+
+// her sınıf veya method her isi yapmayacak
+// her sınıfın veya method spesifik isleri olacak
+// mumkun oldugunca kısa olacak
+// her sınıf ve method iyi tanımlanmıs isler yapacak
---
+++ week_0003/src/main/java/_coupling/Main.java
@@ -0,0 +1,22 @@
+package _coupling;
+
+/*
+ * created by Muhammed Topgul
+ * on 18/10/2020
+ * at 13:10
+ */
+
+public class Main {
+}
+
+// coupling -> baglanti
+// cohesion -> baglılık
+
+// iyi bir OOP icin istenen bir durum
+// loose coupling ve high cohesion
+
+// bir projede N tane class olacak
+// classlar birbirleriyle etkilesimde olacaklar
+
+// COUPLING: classların nasıl birbirleriyle etkilesim kurduguyla ilgilidir veya birbirlerini ne kadar bildikleriyle alakalı bir kavramdır
+// encapsulation, dependency injection veya interface kurallarında uygudugumuzda LOOSE COUPLING'e uymus oluruz
